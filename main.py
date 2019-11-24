a33a2702240a193b2083a19bf1b49b852c4b86e3
Pranit Bauva <pranit.bauva@gmail.com>
2019-11-24 22:13:45 +0530

    main: print details of short listed candidates
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-import tabula import read_pdf
+from tabula import read_pdf
+
+
+def get_map(filename):
+    map_roll_no_to_name = {}
+    with open('students.csv') as f:
+        students_raw = f.read()
+        students = students_raw.split('\n')
+        for student in students:
+            student = student.split(',')
+
+            if student[0] is None or '':
+                continue
+
+            if len(student) < 3:
+                continue
+
+            map_roll_no_to_name[student[0]] = (student[1][3:], student[2])
+
+    return map_roll_no_to_name
+
 
 json = read_pdf('test.pdf', output_format='json')
 json = json[0]
@@ -9,5 +29,13 @@ roll_nos = []
 for row in data:
     for entry in row:
         roll_nos.append(entry['text'])
+
+mapping = get_map('students.csv')
+
+for student in roll_nos:
+    try:
+        print(student + ': ' + str(mapping[student]))
+    except Exception as e:
+        continue
 
 
