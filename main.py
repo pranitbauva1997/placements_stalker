8033a360eaaf4603249f7ce91a2cf7f40f1360e4
Pranit Bauva <pranit.bauva@gmail.com>
2019-12-02 01:11:51 +0530

    add new dataset and update the map
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tabula import read_pdf
 
 def get_map(filename):
     map_roll_no_to_name = {}
+
     with open('students.csv') as f:
         students_raw = f.read()
         students = students_raw.split('\n')
@@ -18,6 +19,21 @@ def get_map(filename):
                 continue
 
             map_roll_no_to_name[student[0]] = (student[1][3:], student[2])
+
+    with open('students_hall.csv') as f:
+        students_raw = f.read()
+        students = students_raw.split('\n')
+        for student in students:
+            student = student.split(',')
+
+            if student[0] is '':
+                continue
+
+            if len(student) < 3:
+                continue
+
+            temp1, temp2 = map_roll_no_to_name[student[0]]
+            map_roll_no_to_name[student[0]] = (temp1, temp2, student[4])
 
     return map_roll_no_to_name
 
